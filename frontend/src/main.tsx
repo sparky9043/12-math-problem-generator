c3fc64353bc3bed60a6b3999989c9072e1de0903
Se Joon Park <sparky9043@gmail.com>
2025-08-06 13:33:27 +0900

    initiate query client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Router>
   </StrictMode>,
 )
