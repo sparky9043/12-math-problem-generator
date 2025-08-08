e7936e7b7086353d54b09beec34e149f6b57aaa2
Se Joon Park <sparky9043@gmail.com>
2025-08-08 16:53:05 +0900

    create a file for current user context

---
+++ frontend/src/contexts/CurrentUser.ts
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const CurrentUserContext = createContext(null)
+
+export const currentUserReducer = (state, action) => {
+  switch (action.type) {
+    
+  }
+}
+
+export default CurrentUserContext
