0906af64ca0b7feaa5aa728e96feca2085373cb0
Se Joon Park <sparky9043@gmail.com>
2025-08-08 14:11:27 +0900

    create styles for sidebar in dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -2,15 +2,15 @@ import { NavLink, Outlet } from "react-router-dom"
 
 const DashBoard = () => {
   return (
-    <div className="h-7/8" id='hello'>
-      <div>
-        <ul>
-          <li>
+    <div className="h-full" id='hello'>
+      <div className="flex flex-col h-full">
+        <ul className="w-1/5 h-full bg-emerald-700 text-emerald-50">
+          <li className="p-2">
             <NavLink to='problems'>
               Problems
             </NavLink>
           </li>
-          <li>
+          <li className="p-2">
             <NavLink to='create'>
               Create Problems
             </NavLink>
