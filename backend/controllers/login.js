a36740a40a2fc343e81a8991005fdd03d902e6a4
Se Joon Park <sparky9043@gmail.com>
2025-08-13 16:26:36 +0900

    create login services file for handling login

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,36 +1,20 @@
 const loginRouter = require('express').Router()
-const User = require('../models/user')
 const userServices = require('../services/userServices')
+const loginServices = require('../services/loginServices')
 const configs = require('../utils/configs')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 loginRouter.post('/', async (request, response) => {
-  const { username, password } = request.body
+  const validUser = await loginServices.isCredentialValid(request)
 
-  const user = await userServices.getUserByUsername(username)
-
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(password, user.passwordHash)
-
-  if (!(user && passwordCorrect)) {
-    return response.status(400).json({ error: 'user or password invalid' })
+  if (!validUser) {
+    return response.status(400).json({ error: 'username or password invalid' })
   }
 
-  const userForToken = {
-    user: user.username,
-    id: user._id,
-  }
+  const loginObject = await loginServices.login(validUser)
 
-  const token = jwt.sign(userForToken,
-    configs.SECRET_KEY,
-    { expiresIn: 60 * 60 },
-  )
-
-  const decodedToken = jwt.verify(token, configs.SECRET_KEY)
-
-  response.status(201).send({ token, username: user.username, name: user.name, id: user._id, expiresIn: decodedToken.exp })
+  return response.status(201).send(loginObject)
 })
 
 module.exports = loginRouter
