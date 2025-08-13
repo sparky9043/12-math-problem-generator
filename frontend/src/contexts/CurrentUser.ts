08a00da7bea0156af51f40551d24358252f1c033
Se Joon Park <sparky9043@gmail.com>
2025-08-13 17:15:50 +0900

    update current user reducer to receive payload and appropriate state types

--- frontend/src/contexts/CurrentUser.ts
+++ frontend/src/contexts/CurrentUser.ts
@@ -2,9 +2,19 @@ import { createContext } from "react";
 
 const CurrentUserContext = createContext(null)
 
-export const currentUserReducer = (state, action) => {
+import type { CurrentUser } from "../App";
+
+interface ActionProp {
+  type: string,
+  payload: CurrentUser,
+}
+
+export const currentUserReducer = (state: CurrentUser | null, action: ActionProp) => {
   switch (action.type) {
-    
+    case "updateCurrentUser":
+      return action.payload
+    default:
+      return state
   }
 }
 
