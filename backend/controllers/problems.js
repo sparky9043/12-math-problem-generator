1cd0fd423c60ccac4210226f93e11996eda44e28
Se Joon Park <sparky9043@gmail.com>
2025-08-13 09:47:25 +0900

    remove unused variables in problems controlelr

--- backend/controllers/problems.js
+++ backend/controllers/problems.js
@@ -1,10 +1,5 @@
 const problemsRouter = require('express').Router()
-const Problem = require('../models/problem')
-const User = require('../models/user')
-const configs = require('../utils/configs')
 const problemServices = require('../services/problemServices')
-const userServices = require('../services/userServices')
-const jwt = require('jsonwebtoken')
 
 problemsRouter.get('/', async (request, response) => {
   const problems = await problemServices.getAll()
