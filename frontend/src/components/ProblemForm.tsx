a2b2e16fa24323cac2f6d72544bb5be04ef69b51
Se Joon Park <sparky9043@gmail.com>
2025-08-13 18:30:37 +0900

    create problem form and add styles to input

--- frontend/src/components/ProblemForm.tsx
+++ frontend/src/components/ProblemForm.tsx
@@ -1,5 +1,13 @@
+import useInput from "../hooks/useInput"
+
 
 const ProblemForm = () => {
+  const subject = useInput('')
+  const branch = useInput('')
+  const topic = useInput('')
+  const question = useInput('')
+  const answer = useInput('')
+  const inputStyles = "border-2 rounded border-emerald-700 hover:border-emerald-500 px-0.5 py-1 focus:outline-emerald-800"
 
   const handleCreateProblem = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -8,6 +16,36 @@ const ProblemForm = () => {
   return (
     <form onSubmit={handleCreateProblem} className="w-full">
       {/* <LoadingSpinner /> */}
+      <div>
+        <label>
+          subject
+        </label>
+        <input className={inputStyles} {...subject} />
+      </div>
+      <div>
+        <label>
+          branch
+        </label>
+        <input className={inputStyles} {...branch} />
+      </div>
+      <div>
+        <label>
+          topic
+        </label>
+        <input className={inputStyles} {...topic} />
+      </div>
+      <div>
+        <label>
+          question
+        </label>
+        <input className={inputStyles} {...question} />
+      </div>
+      <div>
+        <label>
+          answer
+        </label>
+        <input className={inputStyles} {...answer} />
+      </div>
     </form>
   )
 }
