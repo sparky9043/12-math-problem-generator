d02d0f63010b6bef1cf40e8205de72956e80665e
Se Joon Park <sparky9043@gmail.com>
2025-08-14 12:15:12 +0900

    render problem detail if id matches

--- frontend/src/components/ProblemDetails.tsx
+++ frontend/src/components/ProblemDetails.tsx
@@ -1,12 +1,48 @@
-import { useParams } from "react-router-dom"
+import { useQueryClient } from "@tanstack/react-query"
+import { useNavigate, useParams } from "react-router-dom"
+import type { Problem } from "./ProblemsList"
 
 const ProblemDetails = () => {
-  const x = useParams()
+  const client = useQueryClient()
+  const problems: Problem[] | undefined = client.getQueryData(['problems'])
+  const { id } = useParams()
+  const navigate = useNavigate()
 
-  console.log('hello', x)
+  if (!problems) {
+    return (
+      <div>
+        You don't have any problems created!
+      </div>
+    )
+  }
+  const targetProblem = problems.find(problem => problem.id === id)
 
+  console.log(targetProblem)
+
+  if (!targetProblem) {
+    return (
+      <div>
+        Error: Invalid id
+      </div>
+    )
+  }
+
+  const convertToChar = (index: number) => {
+    return String.fromCharCode(index + 65)
+  }
+  
   return  (
-    <div>details</div>
+    <div>
+      <h2>Topic: {targetProblem.topic}</h2>
+      <h3>Branch: {targetProblem.branch}</h3>
+      <h3>{targetProblem.question}</h3>
+      <ul>
+        {targetProblem.choices.map((choice, index) => <li key={choice}>
+          {convertToChar(index)}. {choice}
+        </li>)}
+      </ul>
+      <p>Answer: {targetProblem.answer}</p>
+    </div>
   )
 }
 
