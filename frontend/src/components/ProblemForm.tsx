571cea6bb9ca5c2338a23503435d6dc2c643f12a
Se Joon Park <sparky9043@gmail.com>
2025-08-14 17:40:29 +0900

    get code for select from an external source

--- frontend/src/components/ProblemForm.tsx
+++ frontend/src/components/ProblemForm.tsx
@@ -63,6 +63,30 @@ const ProblemForm = () => {
           </label>
           <input className={inputStyles} {...answer} />
         </div>
+
+        {
+          /*
+            Got the design for select from
+            https://www.material-tailwind.com/docs/html/select
+          */
+        }
+
+        <div className="w-full max-w-sm min-w-[200px]">      
+          <div className="relative">
+            <select
+              className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+            </svg>
+          </div>
+        </div>
+
         <button
           className="bg-emerald-100 px-2 py-1 rounded hover:bg-emerald-200 hover:cursor-pointer"
           type="submit"
