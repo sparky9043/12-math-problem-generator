d9ecdc8033ae7100dbdde3475b4afc645a8b8590
Se Joon Park <sparky9043@gmail.com>
2025-08-18 14:27:40 +0900

    add extra div inside dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -3,21 +3,23 @@ import { NavLink, Outlet } from "react-router-dom"
 const DashBoard = () => {
 
   return (
-    <div className="h-full" id='hello'>
-      <div className="flex h-full">
-        <ul className="w-1/5 h-full bg-emerald-700 text-emerald-50">
-          <li className="p-2">
-            <NavLink to='problems'>
-              Problems
-            </NavLink>
-          </li>
-          <li className="p-2">
-            <NavLink to='create'>
-              Create Problems
-            </NavLink>
-          </li>
-        </ul>
-        <Outlet />
+    <div className="h-full">
+      <div className="h-screen">
+        <div className="flex h-full">
+          <ul className="w-1/5 h-full bg-emerald-700 text-emerald-50">
+            <li className="p-2">
+              <NavLink to='problems'>
+                Problems
+              </NavLink>
+            </li>
+            <li className="p-2">
+              <NavLink to='create'>
+                Create Problems
+              </NavLink>
+            </li>
+          </ul>
+          <Outlet />
+        </div>
       </div>
     </div>
   )
