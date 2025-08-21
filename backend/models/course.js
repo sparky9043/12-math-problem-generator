2115b2c4916330313806a01d886a9da35d5cbf64
Se Joon Park <sparky9043@gmail.com>
2025-08-21 13:54:25 +0900

    add courseSchema and export course model

---
+++ backend/models/course.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  codeHash: {
+    type: String,
+  },
+})
+
+courseSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.codeHash
+  }
+})
+
+module.exports = mongoose.model('Course', courseSchema)
