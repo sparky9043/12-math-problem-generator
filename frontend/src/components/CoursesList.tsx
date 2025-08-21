3adffe425a8183d54b053bea81fa072de186ca32
Se Joon Park <sparky9043@gmail.com>
2025-08-21 15:46:14 +0900

    delete unused import from react-router-dom

--- frontend/src/components/CoursesList.tsx
+++ frontend/src/components/CoursesList.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useMatch, useNavigate } from "react-router-dom"
+import { useMatch, useNavigate } from "react-router-dom"
 import { Outlet } from "react-router-dom"
 
 const CoursesList = () => {
