09e17b5f69aa2e98f5c758d1ce0bd2f7dfa3ec1e
Se Joon Park <sparky9043@gmail.com>
2025-08-25 15:24:44 +0900

    fetch courses and display titles on courses list

--- frontend/src/components/CoursesList.tsx
+++ frontend/src/components/CoursesList.tsx
@@ -1,9 +1,37 @@
+import { useQuery } from "@tanstack/react-query"
 import { useMatch, useNavigate } from "react-router-dom"
 import { Outlet } from "react-router-dom"
+import courseServices from '../services/courses'
+import LoadingSpinner from "./LoadingSpinner"
+import useCurrentUser from "../hooks/useCurrentUser"
+
+interface Course {
+  createdAt: string,
+  id: number,
+  title: string,
+  user: string,
+}
 
 const CoursesList = () => {
   const match = useMatch('/dashboard/courses/create')
   const navigate = useNavigate()
+  const { currentUser } = useCurrentUser()
+  const coursesResults = useQuery({
+    queryFn: courseServices.getAllCourses,
+    queryKey: ['courses'],
+  })
+
+  if (coursesResults.isLoading) {
+    return <LoadingSpinner />
+  }
+
+  if (!currentUser) {
+    navigate('/login')
+  }
+
+  const courses: Course[] = coursesResults.data
+
+  const coursesByUser = courses.filter(course => course?.user === currentUser?.id)
 
   const createCourseLink = () => {
     return (
@@ -30,7 +58,14 @@ const CoursesList = () => {
 
   return (
     <div className="p-2">
-      This is a list of courses
+      <h2>Courses List</h2>
+      <ul>
+        {coursesByUser.map(course => <li key={course.id}>
+          <h3>
+            {course.title}
+          </h3>
+        </li>)}
+      </ul>
       <div>
         {createCourseLink()}
       </div>
