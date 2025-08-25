af721ce0185628fb58e0582466263b53c3d8c397
Se Joon Park <sparky9043@gmail.com>
2025-08-25 14:45:23 +0900

    add some logic to create course forms

--- frontend/src/components/CreateCourse.tsx
+++ frontend/src/components/CreateCourse.tsx
@@ -1,18 +1,33 @@
+import useCurrentUser from "../hooks/useCurrentUser"
+
 const CreateCourse = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    console.log('hello')
+  }
+
+  const { currentUser } = useCurrentUser()
+
+  const user = {
+    username: currentUser,
+    id: currentUser?.id,
   }
 
   return (
     <form onSubmit={handleSubmit}>
       Create Course Form
-
-      <button
-        className="border-2 p-1"
-        type='submit'
-      >
-        submit
-      </button>
+      <div>
+        <label></label>
+        <input />
+      </div>
+      <div>
+        <button
+          className="border-2 p-1"
+          type='submit'
+          >
+          submit
+        </button>
+      </div>
     </form>
   )
 }
