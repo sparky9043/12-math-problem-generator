2416faab77a48c327b29fc3a01ce7a55a90db96f
Se Joon Park <sparky9043@gmail.com>
2025-08-29 12:15:58 +0900

    make course creation inputs

--- frontend/src/components/CreateCourse.tsx
+++ frontend/src/components/CreateCourse.tsx
@@ -13,12 +13,18 @@ const CreateCourse = () => {
     id: currentUser?.id,
   }
 
+  const inputStyles = "border-2 rounded border-emerald-700 hover:border-emerald-500 px-0.5 py-1 focus:outline-emerald-800"
+
   return (
     <form onSubmit={handleSubmit}>
       Create Course Form
       <div>
-        <label></label>
-        <input />
+        <label>course title</label>
+        <input className={inputStyles} />
+      </div>
+      <div>
+        <label>course code</label>
+        <input className={inputStyles} />
       </div>
       <div>
         <button
--- frontend/src/services/courses.ts
+++ frontend/src/services/courses.ts
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
+let token:string
+
 const baseUrl = '/api/courses'
 
 const getAllCourses = async () => {
   const response = await axios.get(baseUrl)
   return response.data
+}
+
+const createCourse = async() => {
+
 }
 
 export default { getAllCourses }
