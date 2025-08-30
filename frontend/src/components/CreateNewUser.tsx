ac9996d5151c73719d827f2ef99d0e7b1651f868
Se Joon Park <sparky9043@gmail.com>
2025-08-30 14:10:57 +0900

    add controlled input elements in create account forms

--- frontend/src/components/CreateNewUser.tsx
+++ frontend/src/components/CreateNewUser.tsx
@@ -1,8 +1,68 @@
+import { useState } from 'react'
+
 const CreateNewUser = () => {
+  const [name, setName] = useState<string>('')
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [confirmPwd, setConfirmPwd] = useState<string>('')
+
+  const inputStyles = 'border-2 border-emerald-800 rounded'
+
   return (
-    <div>
-      Create new user!
-    </div>
+    <form className='p-2 flex flex-col gap-2 items-center h-full font text-lg'>
+      <h2>Account Forms</h2>
+      <div>
+        <label>
+          name
+        </label>
+        <input
+          value={name}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setName(event.target.value)}
+          className={inputStyles}
+          type='text'
+        />
+      </div>
+      <div>
+        <label>
+          username
+        </label>
+        <input 
+          className={inputStyles}
+          value={username}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
+          type='text'
+        />
+      </div>
+      <div>
+        <label>
+          password
+        </label>
+        <input
+          className={inputStyles}
+          value={password}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
+          type='password'
+        />
+      </div>
+      <div>
+        <label>
+          confirm
+        </label>
+        <input
+          className={inputStyles}
+          value={confirmPwd}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setConfirmPwd(event.target.value)}
+          type='password'
+        />
+      </div>
+      <div className='text-center'>
+        <button
+          className='border-2 px-2 py-1 rounded'
+        >
+          create account
+        </button>
+      </div>
+    </form>
   )
 }
 
