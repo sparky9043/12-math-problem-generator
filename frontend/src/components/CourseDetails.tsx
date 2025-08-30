099d14326920477bdc52465c3f11683de264b822
Se Joon Park <sparky9043@gmail.com>
2025-08-30 16:39:11 +0900

    render view problems with all list of problems for each coures

--- frontend/src/components/CourseDetails.tsx
+++ frontend/src/components/CourseDetails.tsx
@@ -1,13 +1,64 @@
+import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import problemServices from '../services/problems'
+import LoadingSpinner from './LoadingSpinner'
+
+interface User {
+  username: string,
+  id: string,
+}
+
+interface Course {
+  title: string,
+  id: string,
+}
+
+export interface Problem {
+  subject: string,
+  branch: string,
+  topic: string,
+  question: string,
+  choices: string[],
+  answer: string,
+  user: User,
+  course: Course,
+  id: string,
+}
 
 const CourseDetails = () => {
-  const { id } = useParams()
+  const { id: courseId } = useParams()
+  const problemResult = useQuery({
+    queryFn: problemServices.getProblems,
+    queryKey: ['problems']
+  })
 
-  console.log(id)
+  if (problemResult.isLoading) {
+    return (
+      <LoadingSpinner />
+    )
+  }
+
+  const allProblems: Problem[]  = problemResult.data
+
+
+  const problemsByCourse: Problem[] = allProblems.filter(problem => problem.course.id === courseId)
 
   return (
     <div>
-      
+      <ul>
+        {problemsByCourse.map((problem, index) =>
+          <li
+            className='p-4 my-1'
+            key={problem.id}
+          >
+            <div>
+              {`${index + 1}.`} {problem.question}
+            </div>
+            <div>
+              Answer: {problem.answer}
+            </div>
+          </li>)}
+      </ul>
     </div>
   )
 }
