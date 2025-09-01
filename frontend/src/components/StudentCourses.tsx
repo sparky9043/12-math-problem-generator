74bd8de497addd12e051834024bebf3526efe8f3
Se Joon Park <sparky9043@gmail.com>
2025-09-01 12:24:31 +0900

    make axios return all students only after filtering

--- frontend/src/components/StudentCourses.tsx
+++ frontend/src/components/StudentCourses.tsx
@@ -1,8 +1,44 @@
+import { useNavigate } from 'react-router-dom'
+import useCurrentUser from '../hooks/useCurrentUser'
+import { useQuery } from '@tanstack/react-query'
+import userServices from '../services/users'
+import LoadingSpinner from './LoadingSpinner'
+
 const StudentCourses = () => {
+  const { currentUser: user } = useCurrentUser()
+  const navigate = useNavigate()
+  const studentResults = useQuery({
+    queryFn: userServices.getStudents,
+    queryKey: ['students']
+  })
+
+  if (user?.userType !== 'student') {
+    return (
+      <div>
+        This is for students. Please logout and try again.
+        <button onClick={() => navigate('/login')}></button>
+      </div>
+    )
+  }
+
+  if (studentResults.isLoading) {
+    return (
+      <LoadingSpinner />
+    )
+  }
+  
+  console.log(studentResults.data)
+
 
   return (
     <div>
       Student View
+
+      <div>
+        <button>
+          add new course
+        </button>
+      </div>
     </div>
   )
 }
--- frontend/src/services/users.ts
+++ frontend/src/services/users.ts
@@ -7,11 +7,34 @@ interface UserCredentials {
   userType: string,
 }
 
+interface Course {
+  title: string,
+  id: string,
+}
+
+interface User {
+  name: string,
+  id: string,
+  userType: string,
+  username: string,
+  courses: Course[],
+}
+
 const baseUrl = '/api/users'
+
+const getStudents = async () => {
+  const response = await axios.get(baseUrl)
+
+  const allUsers: User[] = response.data
+
+  const studentsOnly = allUsers.filter(user => user.userType === 'student')
+  
+  return studentsOnly
+}
 
 const createNewUser = async (credentials: UserCredentials) => {
   const response = await axios.post(baseUrl, credentials)
   return response.data
 }
 
-export default { createNewUser }
+export default { createNewUser, getStudents }
