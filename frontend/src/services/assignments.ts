81894be49ea9895182c75cc3e46785c80006c02a
Se Joon Park <sparky9043@gmail.com>
2025-09-09 15:28:57 +0900

    create assignments services and add create assignment function

---
+++ frontend/src/services/assignments.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import type { Problem } from '../components/ProblemsList'
+
+interface ProblemsObject {
+  problems: Problem[],
+  courseId: string,
+}
+
+const baseUrl = '/api/assignments'
+
+let token:string
+
+export const setToken = (inputToken: string) => {
+  token = `Bearer ${inputToken}`
+}
+
+const createAssignment = async (problemsObject: ProblemsObject) => {
+  const config = {
+    headers: {
+      authorization: token
+    }
+  }
+
+  const response = await axios.post(baseUrl, problemsObject, config)
+
+  return response.data
+}
+
+export default { createAssignment }
