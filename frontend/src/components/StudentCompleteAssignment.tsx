c97954c760b184c3de41065f646892ebd9db6545
Se Joon Park <sparky9043@gmail.com>
2025-09-15 16:33:10 +0900

    render assignments by course

--- frontend/src/components/StudentCompleteAssignment.tsx
+++ frontend/src/components/StudentCompleteAssignment.tsx
@@ -1,9 +1,58 @@
+import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
+import assignmentServices from '../services/assignments'
+import problemServices from '../services/problems'
+import LoadingSpinner from './LoadingSpinner'
+import type { Assignment, Problem } from '../types/types'
 
 const StudentCompleteAssignment = () => {
   const { id: courseId, assignment } = useParams()
 
-  console.log(courseId, assignment)
+  const assignmentResult = useQuery({
+    queryFn: () => assignmentServices.getAssignmentsByCourseId(courseId!),
+    queryKey: ['assignments', courseId],
+    enabled: !!courseId,
+  })
+
+  const problemResult = useQuery({
+    queryFn: problemServices.getProblems,
+    queryKey: ['problems'],
+  })
+
+  if (problemResult.isLoading) {
+    return (
+      <LoadingSpinner />
+    )
+  }
+
+  if (assignmentResult.isLoading) {
+    return (
+      <LoadingSpinner />
+    )
+  }
+
+  const assignmentsByUser: Assignment[] | undefined = assignmentResult.data
+
+  if (assignmentsByUser === undefined) {
+    return (
+      <div>
+        Your teacher doesn't have any assignments created
+      </div>
+    )
+  }
+
+  const targetAssignment: Assignment | undefined = assignmentsByUser
+    && assignmentsByUser.find(a => assignment && a.assignmentTitle.includes(assignment))
+
+  if (targetAssignment) {
+    <div>
+      there was an error
+    </div>
+  }
+
+  // const allProblems: Problem[] = problemResult.data
+
+  // console.log(targetAssignment, problemsByCourseId)
 
   return (
     <div>
