da392b249b47adc7aaa21837b5728f856f10076b
Se Joon Park <sparky9043@gmail.com>
2025-09-16 17:08:42 +0900

    show when the student has completed the assignment

--- frontend/src/components/StudentCourseAssignments.tsx
+++ frontend/src/components/StudentCourseAssignments.tsx
@@ -3,9 +3,11 @@ import { useNavigate, useParams } from 'react-router-dom'
 import assignmentServices from '../services/assignments'
 import LoadingSpinner from './LoadingSpinner'
 import type { Assignment } from '../types/types'
+import useCurrentUser from '../hooks/useCurrentUser'
 
 const StudentCourseAssignments = () => {
   const { id: courseId } = useParams()
+  const { currentUser: currentStudent } = useCurrentUser()
   const navigate = useNavigate()
   const { data: assignmentData, isLoading: assignmentIsLoading, isPending } = useQuery({
     queryFn: () => assignmentServices.getAssignmentsByCourseId(courseId!),
@@ -31,17 +33,25 @@ const StudentCourseAssignments = () => {
     )
   }
 
+  const assignmentCompleteCheck = (assignment: Assignment) => {
+    return assignment?.studentsCompleted.map(s => s.studentId).includes(currentStudent ? currentStudent.id : '')
+  }
+
   return (
     <div className='p-2'>
+      <div>
+        The checkmark ✅ means you already completed the assignment
+      </div>
       <ul>
         {assignments.map(assignment => 
           <li key={assignment.id} className='rounded p-1'>
-            {assignment.assignmentTitle}
-
+            {assignmentCompleteCheck(assignment) ? '✅' : null } {assignment.assignmentTitle}
             <button
               className='border-2 mx-4 p-1 rounded'
               onClick={() => navigate(`${assignment.assignmentTitle}`)}
-            >start</button>
+            >
+              {assignmentCompleteCheck(assignment) ? 'view' : 'start'}
+            </button>
           </li>
         )}
       </ul>
