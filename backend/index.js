c8e5ff7e714ec7d868ed3da3ce9e05ccee1d9b84
Se Joon Park <sparky9043@gmail.com>
2025-09-17 14:57:19 +0900

    move folder outside backend folder

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const logger = require('./utils/logger')
-const configs = require('./utils/configs')
-
-app.listen(configs.PORT, () => {
-  logger.info(`Server running on port ${configs.PORT}`)
-})
