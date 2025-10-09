1b1dff000f2bd46679bfbf85019a2e5702f6e7b4
Se Joon Park <sparky9043@gmail.com>
2025-10-09 16:15:34 +0900

    add shadow to svg using drop-shadow on tailwind

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -2,14 +2,14 @@ import Logo from "./Logo"
 
 const HomePage = () => {
   return (
-    <div className="p-6 text-emerald-900 h-2/5">
+    <div className="p-6 text-emerald-700 h-2/5 text-shadow-md text-shadow-stone-300">
       <div className="text-center">
-        <h2 className="font-bold text-xl">
-          Setup Your Classroom with
+        <h2 className="font-bold text-2xl">
+          Setup Your Classroom <br /> with
         </h2>
         <div className="flex flex-col items-center my-2">
-          <p className="uppercase font-bold">Chalk-Lit</p>
-          <Logo color="dark" size='md' />
+          <p className="uppercase font-bold text-xl">Chalk-Lit</p>
+          <Logo color="dark" size='md' shadow="dark" />
         </div>
       </div>
     </div>
