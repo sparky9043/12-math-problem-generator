9c36cfe2805a5f8534de317d048cbf56d613cf33
Se Joon Park <sparky9043@gmail.com>
2025-10-09 15:08:03 +0900

    add logo svg to nav bar

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,6 +1,9 @@
 const HomePage = () => {
   return (
     <div className="p-2">
+      <h2 className="font-bold">
+        Setup Your Classroom
+      </h2>
       This is the homepage
     </div>
   )
