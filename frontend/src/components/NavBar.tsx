73385885fd91669577c77993ee3b6d730e5e9382
Se Joon Park <sparky9043@gmail.com>
2025-10-09 14:30:18 +0900

    add logo to app

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -19,10 +19,16 @@ const NavBar = (props: NavBarProps) => {
   }, [props, isExpired])
 
   return (
-    <nav className="flex justify-between p-8 h-1/8 items-center bg-linear-to-r from-emerald-400 to-emerald-500 text-emerald-50 text-shadow-3xl">
+    <nav className="flex justify-between p-8 h-1/8 items-center bg-linear-to-r from-emerald-400 to-emerald-500 text-emerald-50 font-semibold text-shadow-md text-shadow-black tracking-wider">
       <Toaster />
       <h2>
-        Logo
+        <div className="w-12">
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>webpack</title>
+            <rect width="24" height="24" fill="transparent" />
+            <path d="M21,16.5a1,1,0,0,1-.53.88l-7.9,4.44a1,1,0,0,1-1.14,0l-7.9-4.44A1,1,0,0,1,3,16.5v-9a1,1,0,0,1,.53-.88l7.9-4.44a1,1,0,0,1,1.14,0l7.9,4.44A1,1,0,0,1,21,7.5v9M12,4.15,5,8.09v7.82l7,3.94,7-3.94V8.09L12,4.15m0,2.08,4.9,2.83L12,11.89,7.1,9.06,12,6.23m5,8.66L13,17.2V13.62l4-2.31v3.58M11,17.2,7,14.89V11.31l4,2.31Z"/>
+          </svg>
+        </div>
       </h2>
       <ul className="flex gap-4 items-center">
         <li>
