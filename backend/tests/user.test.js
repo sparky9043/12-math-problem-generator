a929e38451c4403efbf74df4b19a0b2b466e59ba
Se Joon Park <sparky9043@gmail.com>
2025-10-23 17:12:00 +0900

    test for not found errors and malformatted ids

--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -40,8 +40,33 @@ describe('GET Request', () => {
       .expect('Content-Type', /application\/json/)
 
     assert.strictEqual(response.body.username, helper.defaultUsers[0].username)
-  } )
+  })
+
+  test('returns 404 if user not found', async () => {
+    const nonexistentId = '68805516a921ed387a948322'
+    
+    const response = await api
+      .get(`${baseUrl}/${nonexistentId}`)
+      .expect(404)
+      .expect('Content-Type', /application\/json/)
+    
+    assert(response.body.error.includes('user not found'))
+  })
+
+  test('returns 400 if id is not formatted correctly', async () => {
+    const malformattedId = 'malformattedId'
+
+    const response = await api
+      .get(`${baseUrl}/${malformattedId}`)
+      .expect(400)
+    
+    assert(response.body.error.includes('malformatted id'))
+  })
 })
+
+// describe('POST requests', () => {
+//   test('')
+// })
 
 after(async () => {
   await mongoose.connection.close()
