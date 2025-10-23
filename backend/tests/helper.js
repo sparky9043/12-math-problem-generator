24e6f4fe6c3bca49fd151488384714a95e026ff0
Se Joon Park <sparky9043@gmail.com>
2025-10-23 15:40:04 +0900

    add default users array in helper

---
+++ backend/tests/helper.js
@@ -0,0 +1,18 @@
+// Users
+
+const defaultUsers = [
+  {
+    _id: '68f9cd172ae6607c99632388',
+    __v: 0,
+    username: 'teacher',
+    name: 'Wimbley Heatherton',
+    userType: 'teacher',
+  },
+  {
+    _id: '68f9cca57bd6247d5b27263e',
+    __v: 0,
+    username: 'student',
+    name: 'Cornelius Marksweiter',
+    userType: 'student',
+  }
+]
